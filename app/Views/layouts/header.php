a918825fff1dac6007eadb023c653fbaed445cf9
Andin <andnekaptr@gmail.com>
2023-05-22 21:05:35 +0700

    Upload Source code CodeIgniter4

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-5 ">Welcome to Belajar Bareng Andin</h1>
+    <p class="lead mb-5">Which is more difficult, coding or counting? Not both of them, the difficult one is sharing your knowledge with people without asking for the payment. <br/> <small>- Hudya</small></p>
+    <p>Kunjungi instagram saya disini <a href="https://www.instagram.com/saythename_17/ ">disini</a>.</p>
+</div>
